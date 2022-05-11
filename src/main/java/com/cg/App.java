6b7cb28a63c4e336d91b2cd5d597b99abe0d64ea
Gupta <shalu.a.gupta@capgemini.com>
2022-05-11 18:07:18 +0530

    2nd commit

--- src/main/java/com/cg/App.java
+++ src/main/java/com/cg/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Learning maven" );
+        System.out.println( "push into git repo" );
     }
     public int add(int a, int b) {
     	return a+b;
